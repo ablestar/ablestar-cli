43da28cd9646ea8809d3e6baee0bf0324d2d1309
Joseph <entewshi@gmail.com>
2023-07-03 18:28:54 +0100

    Support importing Custom/Manual Collections in Matrixify format

--- src/import.js
+++ src/import.js
@@ -1,13 +1,14 @@
-import { shopifyRESTApi, shopifyRESTApiSingle } from '../utils/shopify.js';
+import { customCollectionQuery, handleOrIDCheck, itemIDForMatrixify } from '../utils/import.js';
+import { shopifyItemRESTApi, shopifyRESTApi, shopifyRESTApiSingle } from '../utils/shopify.js';
 import cliProgress from 'cli-progress';
 
 export async function runImport(options, fileData) {
-  const bar1 = new cliProgress.SingleBar(
-    {
-      format: `[{bar}] {percentage}% | DUR: {duration}s | ETA: {eta}s | {value}/{total}`,
-    },
-    cliProgress.Presets.shades_classic,
-  );
+	const bar1 = new cliProgress.SingleBar(
+		{
+			format: `[{bar}] {percentage}% | DUR: {duration}s | ETA: {eta}s | {value}/{total}`,
+		},
+		cliProgress.Presets.shades_classic,
+	);
 	try {
 		console.log();
 		bar1.start(fileData.length, 0);
@@ -37,47 +38,232 @@ export async function runImport(options, fileData) {
 				since_id = data[options.type][data[options.type].length - 1].id;
 			}
 
-      const filterCol = options.idColumn === 'email' ? 'email' : 'id';
-      const ids = fileData.map(item => ({
-        ...item,
-        id: result.find(i => i[filterCol] === item[options.idColumn])?.id || null
-      })) 
-      const updateResult = await Promise.all(
-        ids.map(async item => {
-          if (!item.id) return null;
-          const tags =
-            options.action === 'set-tags'
-              ? item[options.actionColumn]
-              : options.action === 'add-tags'
-              ? `${
-                  result.find(i => i.id === item.id)?.tags ||
-                  ''
-                }, ${item[options.actionColumn]}`
-              : result
-                  .find(i => i.id === item.id)
-                  ?.tags.split(', ')
-                  .filter(
-                    el =>
-                      !item[options.actionColumn]
-                        .split(', ')
-                        .includes(el),
-                  );
-          const response = await shopifyRESTApiSingle(
-            options.url,
-            options.type,
-            item.id,
-            'put',
-            { customer: { id: item.id, tags } },
-          );
-          bar1.increment(1);
-          return response.customer;
-        }),
-      );
-      bar1.stop();
-      console.log(updateResult);
+			const filterCol = options.idColumn === 'email' ? 'email' : 'id';
+			const ids = fileData.map(item => ({
+				...item,
+				id: result.find(i => i[filterCol] === item[options.idColumn])?.id || null,
+			}));
+			const updateResult = await Promise.all(
+				ids.map(async item => {
+					if (!item.id) return null;
+					const tags =
+						options.action === 'set-tags'
+							? item[options.actionColumn]
+							: options.action === 'add-tags'
+							? `${result.find(i => i.id === item.id)?.tags || ''}, ${
+									item[options.actionColumn]
+							  }`
+							: result
+									.find(i => i.id === item.id)
+									?.tags.split(', ')
+									.filter(
+										el => !item[options.actionColumn].split(', ').includes(el),
+									);
+					const response = await shopifyRESTApiSingle(
+						options.url,
+						options.type,
+						item.id,
+						'put',
+						{ customer: { id: item.id, tags } },
+					);
+					bar1.increment(1);
+					return response.customer;
+				}),
+			);
+			bar1.stop();
+			console.log(updateResult);
 		}
 	} catch (error) {
-    bar1.stop();
-    throw error;
+		bar1.stop();
+		throw error;
+	}
+}
+
+export async function runImportCustomCollection(options, fileData) {
+	const bar1 = new cliProgress.SingleBar(
+		{
+			format: `[{bar}] {percentage}% | DUR: {duration}s | ETA: {eta}s | {value}/{total}`,
+		},
+		cliProgress.Presets.shades_classic,
+	);
+	try {
+		console.log();
+		bar1.start(fileData.length, 0);
+
+		let since_id = 0;
+		const limit = 250;
+		let result = [];
+
+		let failed = 0,
+			skip = 0,
+			success = 0;
+
+		while (true) {
+			const query = {
+				params: {
+					fields: ['id', 'handle'],
+					since_id,
+					limit,
+				},
+			};
+
+			const data = await shopifyRESTApi(options.url, options.type, 'get', query);
+
+			if (data[options.type].length === 0) break;
+
+			result = [...result, ...data[options.type]];
+
+			if (data[options.type].length !== limit) break;
+			since_id = data[options.type][data[options.type].length - 1].id;
+		}
+
+		for (const item of fileData) {
+			switch (item.Command) {
+				case 'NEW':
+					if (handleOrIDCheck(item, result)) {
+						failed += 1;
+						break;
+					}
+
+					const query = customCollectionQuery(item, result);
+
+					try {
+						await shopifyRESTApi(options.url, options.type, 'post', query);
+						success += 1;
+					} catch (error) {
+						failed += 1;
+					}
+
+					break;
+
+				case 'MERGE':
+					if (handleOrIDCheck(item, result)) {
+						if (!item.ID && !item.Handle) {
+							failed += 1;
+							break;
+						}
+						const query1 = customCollectionQuery(item, result, true);
+						try {
+							await shopifyItemRESTApi(
+								options.url,
+								options.type,
+								itemIDForMatrixify(item, result),
+								'put',
+								query1,
+							);
+							success += 1;
+						} catch (error) {
+							failed += 1;
+						}
+						break;
+					}
+
+					const query1 = customCollectionQuery(item, result);
+					try {
+						await shopifyRESTApi(options.url, options.type, 'post', query1);
+						success += 1;
+					} catch (error) {
+						failed += 1;
+					}
+
+					break;
+
+				case 'UPDATE':
+					if (handleOrIDCheck(item, result)) {
+						if (!item.ID && !item.Handle) {
+							failed += 1;
+							break;
+						}
+						const query1 = customCollectionQuery(item, result, true);
+						try {
+							await shopifyItemRESTApi(
+								options.url,
+								options.type,
+								itemIDForMatrixify(item, result),
+								'put',
+								query1,
+							);
+							success += 1;
+						} catch (error) {
+							failed += 1;
+						}
+						break;
+					}
+
+					failed += 1;
+					break;
+
+				case 'REPLACE':
+					if (handleOrIDCheck(item, result)) {
+						if (!item.ID && !item.Handle) {
+							failed += 1;
+							break;
+						}
+						try {
+							await shopifyItemRESTApi(
+								options.url,
+								options.type,
+								itemIDForMatrixify(item, result),
+								'delete',
+							);
+						} catch (error) {
+							failed += 1;
+							break;
+						}
+					}
+
+					const query2 = customCollectionQuery(item, result);
+					try {
+						await shopifyRESTApi(options.url, options.type, 'post', query2);
+						success += 1;
+					} catch (error) {
+						failed += 1;
+					}
+
+					break;
+
+				case 'DELETE':
+					if (handleOrIDCheck(item, result)) {
+						if (!item.ID && !item.Handle) {
+							failed += 1;
+							break;
+						}
+						try {
+							await shopifyItemRESTApi(
+								options.url,
+								options.type,
+								itemIDForMatrixify(item, result),
+								'delete',
+							);
+							success += 1;
+							break;
+						} catch (error) {
+							failed += 1;
+							break;
+						}
+					}
+
+					failed += 1;
+					break;
+
+				case 'IGNORE':
+					skip += 1;
+					break;
+
+				default:
+					failed += 1;
+					break;
+			}
+			bar1.increment(1);
+		}
+
+		bar1.stop();
+		console.log();
+		console.log(`${success} items success, ${skip} items skip, and ${failed} items failed`);
+		console.log();
+		console.log();
+	} catch (error) {
+		bar1.stop();
+		throw error;
 	}
 }
--- utils/import.js
+++ utils/import.js
@@ -2,16 +2,52 @@ import fs from 'fs';
 import XLSX from 'xlsx';
 
 export async function analyzeFile(fileName) {
-  let fileContent;
-  if (await fs.existsSync(fileName)) {
-    var workbook = XLSX.readFile(fileName);
-    var sheet_name_list = workbook.SheetNames;
-    var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+	let fileContent;
+	if (await fs.existsSync(fileName)) {
+		var workbook = XLSX.readFile(fileName);
+		var sheet_name_list = workbook.SheetNames;
+		var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
-    return xlData;
+		return xlData;
 	} else {
 		console.log();
 		console.log('Please correct file path/name');
 		throw 'Error';
 	}
 }
+
+export const itemIDForMatrixify = (item, result) => {
+	return item.ID && result.map(i => i.id).includes(item.ID)
+		? item.ID
+		: result.find(i => i.handle === item.Handle).id;
+};
+
+export const handleOrIDCheck = (item, result) => {
+	return (
+		(item.ID && result.map(i => i.id).includes(item.ID)) ||
+		(item.Handle && result.map(i => i.handle).includes(item.Handle))
+	);
+};
+
+export const customCollectionQuery = (item, result, showId = false) => {
+	return {
+		custom_collection: {
+			...(showId
+				? {
+						id: itemIDForMatrixify(item, result),
+				  }
+				: {}),
+			handle: item.Handle,
+			title: item.Title,
+			body_html: item['Body HTML'],
+			sort_order: item['Sort Order']?.toLowerCase(),
+			published: item.Published,
+			published_scope: item['Published Scope'],
+			template_suffix: item['Template Suffix'],
+			image: {
+				src: item['Image Src'],
+				alt: item['Image Alt Text'],
+			},
+		},
+	};
+};
